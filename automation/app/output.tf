7624498569419b70553bca6768a7a19fac29856a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-04-30 00:18:58 +0800

    Updated output

--- automation/app/output.tf
+++ automation/app/output.tf
@@ -5,12 +5,18 @@ output "access-info" {
 ACCESS INFORMATION
 ==================
 
-%{for location, ip in local.vm-ips~}
+%{for location, res in local.regional-resources~}
 Location: ${location}
 ---------------------
-  SSH Access:    
-    ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${ip}
+  SSH Access:
+    ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${res.ip}
     ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${aws_route53_record.regional-app[location].name}
+
+  SSM Access:
+    ssh ${res.vm-name}
+    aws ssm start-session --target ${res.instance-id} --region ${res.region}
+    ssh ssm.${res.instance-id}.${res.region}.$AWS_PROFILE
+
   Web Access:
     http://${aws_route53_record.regional-app[location].name}
     https://${aws_route53_record.regional-app[location].name}
@@ -19,6 +25,17 @@ Location: ${location}
 
 %{endfor~}
 
+Start App VMs
+-------------
+%{for location, res in local.regional-resources~}
+aws ec2 start-instances --instance-ids ${res.instance-id} --region ${res.region}
+%{endfor~}
+
+Stop App VMs
+-------------
+%{for location, res in local.regional-resources~}
+aws ec2 stop-instances --instance-ids ${res.instance-id} --region ${res.region}
+%{endfor~}
 Database DNS:
 -------------
 
@@ -33,7 +50,7 @@ Database DNS:
 
   Multi Region
     %{for db-dns in aws_route53_record.multi-region[*]~}
-    ${db-dns.name} -> ${join(",", db-dns.records)} 
+    ${db-dns.name} -> ${join(",", db-dns.records)}
     %{endfor~}
 
     %{for db-dns in aws_route53_record.multi-region-nodes[*]~}
