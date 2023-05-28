614e93551da9a28986278d92a888400b1118a973
sdeveloper <ssaranga@cloudandbeyond.com>
2023-05-28 18:57:35 +0530

    modified roles creation to work on git bash

---
+++ db/init-scripts/init-01.sql
@@ -0,0 +1,12 @@
+create role mercury login password 'mercury123#';
+create database mercury with owner mercury;
+
+create role venus login password 'venus123#';
+create database venus with owner venus;
+
+
+create role uranus login password 'uranus123#';
+create database uranus with owner uranus;
+
+create role neptune login password 'neptuney123#';
+create database neptune with owner neptune;
