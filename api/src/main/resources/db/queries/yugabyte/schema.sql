270adf3e0922552e3ca551fc2620a7e69efd3103
shaharuk-yb <sshaikh@yugabyte.com>
2023-08-23 12:35:41 +0530

    correctng the DDL sequence

--- api/src/main/resources/db/queries/yugabyte/schema.sql
+++ api/src/main/resources/db/queries/yugabyte/schema.sql
@@ -11,36 +11,6 @@ DROP SEQUENCE IF EXISTS TRADE_SYMBOL_SEQ;
 CREATE SEQUENCE ORDER_ID_SEQ CACHE 1000;
 CREATE SEQUENCE TRADE_SYMBOL_SEQ CACHE 1000;
 
-CREATE OR REPLACE VIEW five_days_stock_trend_v AS
-(
-SELECT trade_symbol_id                                          AS symbol_id,
-       ARRAY_AGG(TRUNC(high_price, 2) ORDER BY price_time DESC) AS trend
-FROM trade_symbol_price_historic tsph
-WHERE interval_period = '1DAY'
-GROUP BY trade_symbol_id
-  );
-
-CREATE OR REPLACE VIEW current_stocks_v AS
-(
-SELECT o.user_id,
-       o.preferred_region,
-       o.symbol_id,
-       sum(CASE WHEN trade_type = 'BUY' THEN stock_units ELSE -stock_units END) AS units
-FROM trade_orders o
-GROUP BY o.user_id, o.preferred_region, o.symbol_id
-  );
-
-CREATE OR REPLACE VIEW stock_period_info_v AS
-(
-SELECT trade_symbol_id,
-       high_price,
-       price_time,
-       interval_period
-FROM trade_symbol_price_historic tsph,
-     current_stocks_v c
-WHERE tsph.trade_symbol_id = c.symbol_id
-  );
-
 CREATE TABLE APP_USER
 (
   ID                 SERIAL,
@@ -115,6 +85,37 @@ CREATE TABLE TRADE_SYMBOL_PRICE_HISTORIC
   INTERVAL_PERIOD VARCHAR(6) NOT NULL CHECK ( INTERVAL_PERIOD IN
                                               ('1H', '90MIN', '1DAY', '1WEEK', '3MONTH'))
 );
+
+CREATE OR REPLACE VIEW five_days_stock_trend_v AS
+(
+SELECT trade_symbol_id                                          AS symbol_id,
+       ARRAY_AGG(TRUNC(high_price, 2) ORDER BY price_time DESC) AS trend
+FROM trade_symbol_price_historic tsph
+WHERE interval_period = '1DAY'
+GROUP BY trade_symbol_id
+  );
+
+CREATE OR REPLACE VIEW current_stocks_v AS
+(
+SELECT o.user_id,
+       o.preferred_region,
+       o.symbol_id,
+       sum(CASE WHEN trade_type = 'BUY' THEN stock_units ELSE -stock_units END) AS units
+FROM trade_orders o
+GROUP BY o.user_id, o.preferred_region, o.symbol_id
+  );
+
+CREATE OR REPLACE VIEW stock_period_info_v AS
+(
+SELECT trade_symbol_id,
+       high_price,
+       price_time,
+       interval_period
+FROM trade_symbol_price_historic tsph,
+     current_stocks_v c
+WHERE tsph.trade_symbol_id = c.symbol_id
+  );
+
 TRUNCATE TABLE REF_DATA;
 
 INSERT INTO REF_DATA(KEY_NAME, KEY_VALUE)
