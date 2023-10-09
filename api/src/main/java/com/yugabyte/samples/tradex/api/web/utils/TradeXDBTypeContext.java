780a83abac11fac731f7bf253e3fff30d5f6288c
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-10 01:08:34 +0800

    Add defaults

--- api/src/main/java/com/yugabyte/samples/tradex/api/web/utils/TradeXDBTypeContext.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/web/utils/TradeXDBTypeContext.java
@@ -1,6 +1,7 @@
 package com.yugabyte.samples.tradex.api.web.utils;
 
 import com.yugabyte.samples.tradex.api.config.TradeXDataSourceType;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -11,7 +12,14 @@ public class TradeXDBTypeContext {
 
 
   @Value("${app.datasource_types}")
+  TradeXDataSourceType[] dbTypes;
+
   TradeXDataSourceType dbType;
+
+  @PostConstruct
+  public void init(){
+    this.dbType = dbTypes[0];
+  }
 
   public void setDbType(TradeXDataSourceType dbType) {
     this.dbType = dbType;
