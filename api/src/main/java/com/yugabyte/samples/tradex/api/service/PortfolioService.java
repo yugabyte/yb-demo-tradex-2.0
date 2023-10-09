1061a2175aa88e9235939ad4d088949ca76e2ec2
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-09 22:58:13 +0800

    Cleanup autowiring

--- api/src/main/java/com/yugabyte/samples/tradex/api/service/PortfolioService.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/service/PortfolioService.java
@@ -6,55 +6,72 @@ import com.yugabyte.samples.tradex.api.domain.business.TradeOrderPerformance;
 import com.yugabyte.samples.tradex.api.domain.db.AppUser;
 import com.yugabyte.samples.tradex.api.domain.db.TradeOrder;
 import com.yugabyte.samples.tradex.api.domain.db.TradeXStock;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 @Slf4j
 public class PortfolioService {
-    @Autowired
-    StockInfoService stockInfoService;
-    @Autowired
-    TradeService tradeService;
-    @Autowired
-    UserService userService;
 
-    public Portfolio getPortfolio(TradeXDataSourceType dbType, String email) {
+  private final StockInfoService stockInfoService;
+  private final TradeService tradeService;
+  private final UserService userService;
 
-        Optional<AppUser> user = userService.findByEmail(dbType, email);
+  public PortfolioService(StockInfoService stockInfoService, TradeService tradeService,
+    UserService userService) {
+    this.stockInfoService = stockInfoService;
+    this.tradeService = tradeService;
+    this.userService = userService;
+  }
 
-        int userId = user.get().getId().getId();
-        String prefRegion = user.get().getId().getPreferredRegion();
+  public Portfolio getPortfolio(TradeXDataSourceType dbType, String email) {
 
-        log.debug("resolved datasource type: {}", dbType);
-        List<TradeOrder> recentTradeOrders = tradeService.fetchByUserAndLimit(dbType, userId, 0, 20, prefRegion);
-        Portfolio userPortfolio = new Portfolio();
-        userPortfolio.setUserId(user.get().getId());
-        userPortfolio.setEmail(user.get().getEmail());
-        userPortfolio.setRecentTrades(recentTradeOrders.stream().map(e -> getTradePerformance(dbType, e)).collect(Collectors.toList()));
-        log.debug("Fetched details: {}", userPortfolio);
-        return userPortfolio;
-    }
+    Optional<AppUser> user = userService.findByEmail(dbType, email);
 
-    private TradeOrderPerformance getTradePerformance(TradeXDataSourceType dbType, TradeOrder tradeOrder) {
-        TradeOrderPerformance tradeOrderPerformance = new TradeOrderPerformance();
-        TradeXStock stock = tradeOrder.getStock();
-        tradeOrderPerformance.setStockCompany(stock.getCompany());
-        tradeOrderPerformance.setOrderId(tradeOrder.getId());
-        tradeOrderPerformance.setStockSymbol(stock.getSymbol());
-        tradeOrderPerformance.setCurrentPrice(stock.getHighPrice() == null ? 0.0 : stock.getHighPrice().doubleValue());
-        tradeOrderPerformance.setProfit(tradeOrder.getBidPrice() < stock.getHighPrice().doubleValue());
-        tradeOrderPerformance.setProfitPercent((stock.getHighPrice().doubleValue() - tradeOrder.getBidPrice()) / 100);
-        tradeOrderPerformance.setTrend(stockInfoService.fetchStockTrend(tradeOrder.getStock().getId()));
+    int userId = user.get()
+      .getId()
+      .getId();
+    String prefRegion = user.get()
+      .getId()
+      .getPreferredRegion();
 
-        return tradeOrderPerformance;
-    }
+    log.debug("resolved datasource type: {}", dbType);
+    List<TradeOrder> recentTradeOrders = tradeService.fetchByUserAndLimit(dbType, userId, 0, 20,
+      prefRegion);
+    Portfolio userPortfolio = new Portfolio();
+    userPortfolio.setUserId(user.get()
+      .getId());
+    userPortfolio.setEmail(user.get()
+      .getEmail());
+    userPortfolio.setRecentTrades(recentTradeOrders.stream()
+      .map(e -> getTradePerformance(dbType, e))
+      .collect(Collectors.toList()));
+    log.debug("Fetched details: {}", userPortfolio);
+    return userPortfolio;
+  }
+
+  private TradeOrderPerformance getTradePerformance(TradeXDataSourceType dbType,
+    TradeOrder tradeOrder) {
+    TradeOrderPerformance tradeOrderPerformance = new TradeOrderPerformance();
+    TradeXStock stock = tradeOrder.getStock();
+    tradeOrderPerformance.setStockCompany(stock.getCompany());
+    tradeOrderPerformance.setOrderId(tradeOrder.getId());
+    tradeOrderPerformance.setStockSymbol(stock.getSymbol());
+    tradeOrderPerformance.setCurrentPrice(stock.getHighPrice() == null ? 0.0 : stock.getHighPrice()
+      .doubleValue());
+    tradeOrderPerformance.setProfit(tradeOrder.getBidPrice() < stock.getHighPrice()
+      .doubleValue());
+    tradeOrderPerformance.setProfitPercent((stock.getHighPrice()
+                                              .doubleValue() - tradeOrder.getBidPrice()) / 100);
+    tradeOrderPerformance.setTrend(stockInfoService.fetchStockTrend(tradeOrder.getStock()
+      .getId()));
+
+    return tradeOrderPerformance;
+  }
 
 }
