af32cc790fa73d602f237b779e1113c7e715f490
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-09 23:28:24 +0800

    Autowiring cleanup

--- api/src/main/java/com/yugabyte/samples/tradex/api/web/WebMvcConfig.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/web/WebMvcConfig.java
@@ -3,8 +3,8 @@ package com.yugabyte.samples.tradex.api.web;
 
 import com.yugabyte.samples.tradex.api.web.utils.TradeXDBTypeContext;
 import com.yugabyte.samples.tradex.api.web.utils.TradeXDbResolvingInterceptor;
+import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,49 +15,50 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
-
 @Configuration
 @Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.max_age}")
-    private final Long maxAge = 3600L;
+  @Value("${app.cors.max_age}")
+  private final Long maxAge = 3600L;
 //  @Value("${app.cors.allowed_origins}")
 //  private String[] allowedOrigins;
 
-    @Autowired
-    TradeXDBTypeContext tradeXDBTypeContext;
+  private final TradeXDBTypeContext tradeXDBTypeContext;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+  public WebMvcConfig(TradeXDBTypeContext tradeXDBTypeContext) {
+    this.tradeXDBTypeContext = tradeXDBTypeContext;
+  }
 
-        //  log.info("Using allowedOrigin: {}", Arrays.toString(allowedOrigins));
-        registry.addMapping("/**")
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+
+    //  log.info("Using allowedOrigin: {}", Arrays.toString(allowedOrigins));
+    registry.addMapping("/**")
 //            .allowedOrigins(allowedOrigins)
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(maxAge);
-    }
+      .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+      .maxAge(maxAge);
+  }
 
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(Locale.ENGLISH);
-        return localeResolver;
-    }
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.ENGLISH);
+    return localeResolver;
+  }
 
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("lang");
-        return localeChangeInterceptor;
-    }
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("lang");
+    return localeChangeInterceptor;
+  }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TradeXDbResolvingInterceptor(tradeXDBTypeContext));
-        registry.addInterceptor(localeChangeInterceptor());
-    }
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new TradeXDbResolvingInterceptor(tradeXDBTypeContext));
+    registry.addInterceptor(localeChangeInterceptor());
+  }
 
 }
