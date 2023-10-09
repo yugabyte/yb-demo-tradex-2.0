d55126f8fa69b4d1477c75569da8dd7eb16667eb
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-09 23:28:46 +0800

    Autowiring cleanup

--- api/src/main/java/com/yugabyte/samples/tradex/api/StartupTask.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/StartupTask.java
@@ -6,91 +6,99 @@ import com.yugabyte.samples.tradex.api.service.ApplicationServiceException;
 import com.yugabyte.samples.tradex.api.service.RefDataService;
 import com.yugabyte.samples.tradex.api.service.StockInfoService;
 import com.yugabyte.samples.tradex.api.service.TradeService;
-import com.yugabyte.samples.tradex.api.utils.TradeXJdbcTemplateResolver;
+import com.yugabyte.samples.tradex.api.utils.Sql;
 import com.yugabyte.samples.tradex.api.web.utils.TradesGenerator;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-
 @Component
 @Slf4j
 public class StartupTask implements CommandLineRunner {
 
-    static final String CHECK_SQL = "select count(*) from REF_DATA";
-    final List<Integer> sampleStockList = List.of(43, 67, 5, 66, 70, 22);
-    @Autowired
-    RefDataService dataService;
-    @Autowired
-    StockInfoService stockInfoService;
-    @Autowired
-    TradeService tradeService;
-    @Autowired
-    TradeXJdbcTemplateResolver jdbcTemplateResolver;
-    @Value("${app.load_mock_data}")
-    boolean loadRequired;
-    @Value("${app.mylocation}")
-    String location;
-    @Autowired
-    TradesGenerator generator;
+  private final List<Integer> sampleStockList = List.of(43, 67, 5, 66, 70, 22);
+  private final RefDataService dataService;
+  private final StockInfoService stockInfoService;
+  private final TradeService tradeService;
+  private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    @Value("${app.datasource_types}")
-    TradeXDataSourceType[] tradeXInputDataSourceTypes;
+  @Value("${app.load_mock_data}")
+  boolean loadMockData;
 
-    @Override
-    public void run(String... args) throws Exception {
-        Instant start = Instant.now();
-        NamedParameterJdbcTemplate template = jdbcTemplateResolver.resolve(TradeXDataSourceType.MULTI_REGION_MULTI_ZONE);
-        log.info("Application started. Number of REF_DATA entries: "
-                + template.queryForObject(CHECK_SQL, new HashMap<>(0), Integer.class));
-        try {
+  @Value("${app.mylocation}")
+  String location;
+  final TradesGenerator generator;
 
-            if (loadRequired) {
-                log.info("Loading metadata - begin");
-                stockInfoService.loadStockPerformance(false);
-                for (TradeXDataSourceType t : tradeXInputDataSourceTypes) {
-                    dataService.getDBNodes(t);
-                    dataService.getDbClusterTypes(t);
-                    dataService.getTrafficLocations(t);
+  @Value("${app.datasource_types}")
+  TradeXDataSourceType[] tradeXInputDataSourceTypes;
 
-                    // generate few trade orders
-                    if ("BOSTON".equalsIgnoreCase(location)) {
-                        List<TradeOrder> user1Trades = generator.generateTrades(354, 1, "us-east-1", sampleStockList);
-                        List<TradeOrder> user2Trades = generator.generateTrades(354, 2, "us-east-1", sampleStockList);
-                        tradeService.deleteAllUserTrades(t, 1);
-                        tradeService.deleteAllUserTrades(t, 2);
-                        tradeService.insertTrades(t, user1Trades, 1, "us-east-1");
-                        tradeService.insertTrades(t, user2Trades, 2, "us-east-1");
-                    }
-                  if ("WASHINGTON".equalsIgnoreCase(location)) {
-                    List<TradeOrder> user1Trades = generator.generateTrades(354, 1, "us-west-2", sampleStockList);
-                    List<TradeOrder> user2Trades = generator.generateTrades(354, 2, "us-west-2", sampleStockList);
-                    tradeService.deleteAllUserTrades(t, 1);
-                    tradeService.deleteAllUserTrades(t, 2);
-                    tradeService.insertTrades(t, user1Trades, 1, "us-west-2");
-                    tradeService.insertTrades(t, user2Trades, 2, "us-west-2");
-                  }
-                }
-                dataService.getNodeLocations();
+  public StartupTask(RefDataService dataService, StockInfoService stockInfoService,
+    TradeService tradeService, NamedParameterJdbcTemplate jdbcTemplate, TradesGenerator generator) {
+    this.dataService = dataService;
+    this.stockInfoService = stockInfoService;
+    this.tradeService = tradeService;
+    this.jdbcTemplate = jdbcTemplate;
+    this.generator = generator;
+  }
 
-                log.info("Loading metadata - complete in {} millisecs", Duration.between(start, Instant.now()).toMillis());
+  @Override
+  public void run(String... args) throws Exception {
+    Instant start = Instant.now();
+    log.info(
+      "Application started. Number of REF_DATA entries: " + jdbcTemplate.queryForObject(Sql.RefData.CHECK_SQL,
+        new HashMap<>(0), Integer.class));
+    try {
 
-            }
+      if (loadMockData) {
+        log.info("Loading metadata - begin");
+        stockInfoService.loadStockPerformance(false);
+        for (TradeXDataSourceType t : tradeXInputDataSourceTypes) {
+          dataService.getDBNodes(t);
+          dataService.getDbClusterTypes(t);
+          dataService.getTrafficLocations(t);
 
-            //load Stock Cache
-
-
-        } catch (ApplicationServiceException e) {
-            e.printStackTrace();
+          // generate few trade orders
+          if ("BOSTON".equalsIgnoreCase(location)) {
+            List<TradeOrder> user1Trades = generator.generateTrades(354, 1, "us-east-1",
+              sampleStockList);
+            List<TradeOrder> user2Trades = generator.generateTrades(354, 2, "us-east-1",
+              sampleStockList);
+            tradeService.deleteAllUserTrades(t, 1);
+            tradeService.deleteAllUserTrades(t, 2);
+            tradeService.insertTrades(t, user1Trades, 1, "us-east-1");
+            tradeService.insertTrades(t, user2Trades, 2, "us-east-1");
+          }
+          if ("WASHINGTON".equalsIgnoreCase(location)) {
+            List<TradeOrder> user1Trades = generator.generateTrades(354, 1, "us-west-2",
+              sampleStockList);
+            List<TradeOrder> user2Trades = generator.generateTrades(354, 2, "us-west-2",
+              sampleStockList);
+            tradeService.deleteAllUserTrades(t, 1);
+            tradeService.deleteAllUserTrades(t, 2);
+            tradeService.insertTrades(t, user1Trades, 1, "us-west-2");
+            tradeService.insertTrades(t, user2Trades, 2, "us-west-2");
+          }
         }
+        dataService.getNodeLocations();
+
+        log.info("Loading metadata - complete in {} millisecs",
+          Duration.between(start, Instant.now())
+            .toMillis());
+
+      }
+
+      //load Stock Cache
+
+    } catch (ApplicationServiceException e) {
+      e.printStackTrace();
     }
+  }
 
 
 }
