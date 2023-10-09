97c59287e2de035b7beb9ac50718b952784988cb
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-09 23:29:50 +0800

    Autowire cleanup

--- api/src/main/java/com/yugabyte/samples/tradex/api/web/controllers/ChartDataController.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/web/controllers/ChartDataController.java
@@ -1,6 +1,7 @@
 package com.yugabyte.samples.tradex.api.web.controllers;
 
 import com.yugabyte.samples.tradex.api.domain.db.AppUser;
+import com.yugabyte.samples.tradex.api.domain.repo.ConnectionInfoRepo;
 import com.yugabyte.samples.tradex.api.service.ChartDataService;
 import com.yugabyte.samples.tradex.api.service.DBOperationResult;
 import com.yugabyte.samples.tradex.api.service.UserService;
@@ -8,7 +9,6 @@ import com.yugabyte.samples.tradex.api.web.utils.TradeXDBTypeContext;
 import com.yugabyte.samples.tradex.api.web.utils.WebConstants;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ChartDataController extends BaseController {
 
-  @Autowired
-  ChartDataService chartDataService;
-  @Autowired
-  UserService userService;
+  private final ChartDataService chartDataService;
 
-  @Autowired
-  TradeXDBTypeContext tradeXDBTypeContext;
+  public ChartDataController(UserService userService, ConnectionInfoRepo connectionInfoRepo,
+    TradeXDBTypeContext tradeXDBTypeContext,  ChartDataService chartDataService) {
+    super(userService, connectionInfoRepo, tradeXDBTypeContext);
+    this.chartDataService = chartDataService;
+  }
 
   @GetMapping("/api/charts/portfolio-chart")
   @SecurityRequirement(name = "auth-header-bearer")
   public DBOperationResult getPortfolioChart(Authentication authentication,
-    @RequestHeader(value = WebConstants.TRADEX_QUERY_ANALYZE_HEADER,
-      required = false, defaultValue = "false") Boolean inspectQueries) {
+    @RequestHeader(value = WebConstants.TRADEX_QUERY_ANALYZE_HEADER, required = false, defaultValue = "false") Boolean inspectQueries) {
 
     AppUser appUser = fetchUser(authentication);
     return chartDataService.fetchPortfolioChart(tradeXDBTypeContext.getDbType(), appUser.getId(),
