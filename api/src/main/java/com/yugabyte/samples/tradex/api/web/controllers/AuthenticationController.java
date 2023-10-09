61c8f7b5e3086cb0150ddc722353d9a08712e375
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-09 23:29:38 +0800

    Autowire cleanup

--- api/src/main/java/com/yugabyte/samples/tradex/api/web/controllers/AuthenticationController.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/web/controllers/AuthenticationController.java
@@ -7,129 +7,152 @@ import com.yugabyte.samples.tradex.api.domain.business.SignUpRequest;
 import com.yugabyte.samples.tradex.api.domain.business.SignUpResponse;
 import com.yugabyte.samples.tradex.api.domain.db.AppUser;
 import com.yugabyte.samples.tradex.api.domain.db.AppUserId;
+import com.yugabyte.samples.tradex.api.domain.repo.ConnectionInfoRepo;
 import com.yugabyte.samples.tradex.api.events.NewUserCreatedEventPublisher;
 import com.yugabyte.samples.tradex.api.service.UserService;
 import com.yugabyte.samples.tradex.api.web.ApiException;
 import com.yugabyte.samples.tradex.api.web.security.JwtAuthHelper;
 import com.yugabyte.samples.tradex.api.web.utils.TradeXDBTypeContext;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import java.util.Optional;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
-public class AuthenticationController {
+public class AuthenticationController extends BaseController {
 
-    @Value("${app.mylocation}")
-    String instanceLocation;
-    @Autowired
-    NewUserCreatedEventPublisher publisher;
-    @Autowired
-    private JwtAuthHelper authHelper;
-    @Autowired
-    private UserService userService;
+  @Value("${app.mylocation}")
+  private String instanceLocation;
+  private final NewUserCreatedEventPublisher publisher;
+  private final JwtAuthHelper authHelper;
+  private final UserService userService;
 
-    @Autowired
-    private TradeXDBTypeContext tradeXDBTypeContext;
+  @Autowired
+  private TradeXDBTypeContext tradeXDBTypeContext;
 
-    @PostMapping("/sign-up")
-    @ResponseStatus(HttpStatus.CREATED)
-    public SignUpResponse signup(@Valid @RequestBody SignUpRequest form) {
-        AppUser appUser = createAppUser(form);
+  public AuthenticationController(UserService userService, ConnectionInfoRepo connectionInfoRepo,
+    TradeXDBTypeContext tradeXDBTypeContext, NewUserCreatedEventPublisher publisher,
+    JwtAuthHelper authHelper, UserService userService1) {
+    super(userService, connectionInfoRepo, tradeXDBTypeContext);
+    this.publisher = publisher;
+    this.authHelper = authHelper;
+    this.userService = userService1;
+  }
 
-        return SignUpResponse.builder()
-                .id(appUser.getId().getId())
-                .prefRegion(appUser.getId().getPreferredRegion())
-                .login(appUser.getEmail())
-                .status("SUCCESS")
-                .build();
+  @PostMapping("/sign-up")
+  @ResponseStatus(HttpStatus.CREATED)
+  public SignUpResponse signup(@Valid @RequestBody SignUpRequest form) {
+    AppUser appUser = createAppUser(form);
+
+    return SignUpResponse.builder()
+      .id(appUser.getId()
+        .getId())
+      .prefRegion(appUser.getId()
+        .getPreferredRegion())
+      .login(appUser.getEmail())
+      .status("SUCCESS")
+      .build();
+  }
+
+  @PostMapping("/password-reset")
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public PasswordResetResponse passwordReset(@Valid @RequestBody PasswordResetRequest request) {
+    TradeXDataSourceType dbType = tradeXDBTypeContext.getDbType();
+    String login = request.login();
+    String newPassword = login + "123";
+    final AppUser appUser = userService.findByEmail(dbType, login)
+      .orElseThrow();
+    String newEncodedPassword = authHelper.encodePassword(newPassword);
+
+    var updatePassKeyResult = userService.updatePasskey(dbType, login, newEncodedPassword, appUser.getId()
+      .getPreferredRegion());
+
+    log.info("{}: Password reset successful. Result: {}", login, updatePassKeyResult);
+
+    return new PasswordResetResponse(login, newPassword, true);
+  }
+
+  @PostMapping("/sign-in")
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public AuthenticationResponse authenticate(@Valid @RequestBody AuthenticationRequest request) {
+
+    String jwt = authHelper.processLoginAndGenerateJwt(request.getLogin(),
+      request.getCredentials());
+    Optional<AppUser> appUser = userService.findByEmail(tradeXDBTypeContext.getDbType(),
+      request.getLogin());
+
+    if (appUser.isEmpty()) {
+      log.error("No user with username: {}", request.getLogin());
+      throw new UsernameNotFoundException("No user with username " + request.getLogin());
     }
 
-    @PostMapping("/password-reset")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public PasswordResetResponse passwordReset(@Valid @RequestBody PasswordResetRequest request) {
-        TradeXDataSourceType dbType = tradeXDBTypeContext.getDbType();
-        String login = request.login();
-        String newPassword = login + "123";
-        final AppUser appUser = userService.findByEmail(dbType, login)
-                .orElseThrow();
-        String newEncodedPassword = authHelper.encodePassword(newPassword);
+    AppUserId userId = appUser.get()
+      .getId();
 
-        userService.updatePasskey(dbType, login, newEncodedPassword, appUser.getId().getPreferredRegion());
-        log.info("{}: Password reset successful", login);
-        return new PasswordResetResponse(login, newPassword, true);
+    return AuthenticationResponse.builder()
+      .token(jwt)
+      .type("Bearer")
+      .status("SUCCESS")
+      .id(userId.getId())
+      .preferredRegion(userId.getPreferredRegion())
+      .build();
+  }
+
+  @PostMapping("/sign-out")
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  @SecurityRequirement(name = "auth-header-bearer")
+  public MessageResponse signOut() {
+    log.info("Got sign out request");
+    return new MessageResponse("Sign Out Successful", true);
+  }
+
+  private AppUser createAppUser(SignUpRequest form) {
+
+    TradeXDataSourceType dbType = tradeXDBTypeContext.getDbType();
+
+    if (userService.existsByEmail(dbType, form.getEmail())) {
+      throw new ApiException("Failed to complete signup", "email", form.getEmail(),
+        "Email already in use");
     }
 
-    @PostMapping("/sign-in")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public AuthenticationResponse authenticate(@Valid @RequestBody AuthenticationRequest request) {
+    AppUser newUser = new AppUser();
+    BeanUtils.copyProperties(form, newUser);
+    newUser.setPasskey(authHelper.encodePassword(form.getPasskey()));
+    AppUserId generatedUserId = (AppUserId) userService.createNewUser(dbType, newUser,
+        instanceLocation)
+      .getData();
 
-        String jwt = authHelper.processLoginAndGenerateJwt(request.getLogin(), request.getCredentials());
-        Optional<AppUser> appUser = userService.findByEmail(tradeXDBTypeContext.getDbType(), request.getLogin());
+    AppUser appUser = userService.findByAppUserId(dbType, generatedUserId)
+      .get();
+    publisher.publishCustomEvent(appUser, dbType);
 
-        if (appUser.isEmpty()) {
-            log.error("No user with username: {}", request.getLogin());
-            throw new UsernameNotFoundException("No user with username " + request.getLogin());
-        }
+    return appUser;
+  }
 
-        AppUserId userId = appUser.get().getId();
+  public record PasswordResetRequest(String login) {
 
-        return AuthenticationResponse.builder()
-                .token(jwt)
-                .type("Bearer")
-                .status("SUCCESS")
-                .id(userId.getId())
-                .preferredRegion(userId.getPreferredRegion())
-                .build();
-    }
+  }
 
-    @PostMapping("/sign-out")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    @SecurityRequirement(name = "auth-header-bearer")
-    public MessageResponse signOut() {
-        log.info("Got sign out request");
-        return new MessageResponse("Sign Out Successful", true);
-    }
+  public record PasswordResetResponse(String login, String password, boolean status) {
 
-    private AppUser createAppUser(SignUpRequest form) {
+  }
 
-        TradeXDataSourceType dbType = tradeXDBTypeContext.getDbType();
+  public record MessageResponse(String message, boolean status) {
 
-        if (userService.existsByEmail(dbType, form.getEmail())) {
-            throw new ApiException("Failed to complete signup", "email", form.getEmail(), "Email already in use");
-        }
-
-        AppUser newUser = new AppUser();
-        BeanUtils.copyProperties(form, newUser);
-        newUser.setPasskey(authHelper.encodePassword(form.getPasskey()));
-        AppUserId generatedUserId = (AppUserId) userService.createNewUser(dbType, newUser, instanceLocation).getData();
-
-        AppUser appUser = userService.findByAppUserId(dbType, generatedUserId).get();
-        publisher.publishCustomEvent(appUser, dbType);
-
-        return appUser;
-    }
-
-    public record PasswordResetRequest(String login) {
-
-    }
-
-    public record PasswordResetResponse(String login, String password, boolean status) {
-
-    }
-
-    public record MessageResponse(String message, boolean status) {
-
-    }
+  }
 }
