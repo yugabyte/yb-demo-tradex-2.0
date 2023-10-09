b707b95dd85bd5fa58aeb72c2af221e24263585e
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-10 01:08:25 +0800

    Autowire cleanup

--- api/src/main/java/com/yugabyte/samples/tradex/api/utils/TradeXJdbcTemplateResolver.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/utils/TradeXJdbcTemplateResolver.java
@@ -1,40 +1,45 @@
 package com.yugabyte.samples.tradex.api.utils;
 
 import com.yugabyte.samples.tradex.api.config.TradeXDataSourceType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TradeXJdbcTemplateResolver {
 
-    @Autowired(required = false)
-    @Qualifier("SINGLE_DB_TEMPLATE")
-    NamedParameterJdbcTemplate singleDbJdbcTemplate;
+  private final NamedParameterJdbcTemplate singleDbJdbcTemplate;
 
-    @Autowired(required = false)
-    @Qualifier("MULTI_REGION_DB_TEMPLATE")
-    NamedParameterJdbcTemplate multiRegionTemplate;
+  private final NamedParameterJdbcTemplate multiRegionTemplate;
 
-    @Autowired(required = false)
-    @Qualifier("MULTI_REGION_READ_REPLICA_DB_TEMPLATE")
-    NamedParameterJdbcTemplate multiRegionReplicaTemplate;
+  private final NamedParameterJdbcTemplate multiRegionReplicaTemplate;
 
 
-    @Autowired(required = false)
-    @Qualifier("GEO_PARTITIONED_DB_TEMPLATE")
-    NamedParameterJdbcTemplate geoDbJdbcTemplate;
+  private final NamedParameterJdbcTemplate geoDbJdbcTemplate;
+
+  @Value("${app.datasource_types}")
+  private TradeXDataSourceType[] tradeXInputDataSourceTypes;
+
+  public TradeXJdbcTemplateResolver(
+    @Qualifier("SINGLE_DB_TEMPLATE") NamedParameterJdbcTemplate singleDbJdbcTemplate,
+    @Qualifier("MULTI_REGION_DB_TEMPLATE") NamedParameterJdbcTemplate multiRegionTemplate,
+    @Qualifier("MULTI_REGION_READ_REPLICA_DB_TEMPLATE") NamedParameterJdbcTemplate multiRegionReplicaTemplate,
+    @Qualifier("GEO_PARTITIONED_DB_TEMPLATE") NamedParameterJdbcTemplate geoDbJdbcTemplate) {
+    this.singleDbJdbcTemplate = singleDbJdbcTemplate;
+    this.multiRegionTemplate = multiRegionTemplate;
+    this.multiRegionReplicaTemplate = multiRegionReplicaTemplate;
+    this.geoDbJdbcTemplate = geoDbJdbcTemplate;
+  }
 
 
-    public NamedParameterJdbcTemplate resolve(TradeXDataSourceType dbType) {
-        return switch (dbType) {
-            case SINGLE_REGION_MULTI_ZONE -> singleDbJdbcTemplate;
-            case MULTI_REGION_MULTI_ZONE -> multiRegionTemplate;
-            case MULTI_REGION_READ_REPLICA -> multiRegionReplicaTemplate;
-            case GEO_PARTITIONED -> geoDbJdbcTemplate;
-            //case LOCAL -> localJdbcTemplate;
-            //default -> throw new IllegalStateException("Unexpected value: " + dbType);
-        };
-    }
+  public NamedParameterJdbcTemplate resolve(TradeXDataSourceType dbType) {
+    var d = (dbType == null) ? tradeXInputDataSourceTypes[0] : dbType;
+    return switch (d) {
+      case SINGLE_REGION_MULTI_ZONE -> singleDbJdbcTemplate;
+      case MULTI_REGION_MULTI_ZONE -> multiRegionTemplate;
+      case MULTI_REGION_READ_REPLICA -> multiRegionReplicaTemplate;
+      case GEO_PARTITIONED -> geoDbJdbcTemplate;
+    };
+  }
 }
