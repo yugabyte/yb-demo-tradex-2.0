87e2ddf22359af29d7e189c347d61dcd3dd5bcec
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-09 22:58:19 +0800

    Cleanup autowiring

--- api/src/main/java/com/yugabyte/samples/tradex/api/service/QueryAnalysisService.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/service/QueryAnalysisService.java
@@ -2,20 +2,22 @@ package com.yugabyte.samples.tradex.api.service;
 
 import com.yugabyte.samples.tradex.api.config.TradeXDataSourceType;
 import com.yugabyte.samples.tradex.api.domain.repo.ExplainQueryRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class QueryAnalysisService {
 
-    @Autowired
-    ExplainQueryRepo explainQueryRepo;
+  private final ExplainQueryRepo explainQueryRepo;
 
-    public List<String> analyzeQuery(TradeXDataSourceType dbType, String query, MapSqlParameterSource params) {
-        return explainQueryRepo.analyzeQuery(dbType, query, params);
-    }
+  public QueryAnalysisService(ExplainQueryRepo explainQueryRepo) {
+    this.explainQueryRepo = explainQueryRepo;
+  }
+
+  public List<String> analyzeQuery(TradeXDataSourceType dbType, String query,
+    MapSqlParameterSource params) {
+    return explainQueryRepo.analyzeQuery(dbType, query, params);
+  }
 
 }
