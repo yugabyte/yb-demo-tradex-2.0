bb41500b0b8ee9ac59f04bf4a6288b1756f9001a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-10-10 01:07:27 +0800

    Added noarg constructor

--- api/src/main/java/com/yugabyte/samples/tradex/api/domain/business/AuthenticationRequest.java
+++ api/src/main/java/com/yugabyte/samples/tradex/api/domain/business/AuthenticationRequest.java
@@ -1,11 +1,15 @@
 package com.yugabyte.samples.tradex.api.domain.business;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AuthenticationRequest {
 
